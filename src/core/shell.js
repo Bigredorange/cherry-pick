55ff5cdc20d03c10db8fd09ca9fd021e143cd521
cdh <chengdonghai1@gmail.com>
2019-07-12 17:56:24 +0800

    shelljs

--- src/core/shell.js
+++ src/core/shell.js
@@ -1,6 +1,23 @@
-const shelljs = require('shelljs');
+const shell = require('shelljs');
 
-const child = shelljs.exec('ls', { async: true });
-child.stdout.on('data', (data) => {
-  console.log(data);
+let logStr = '';
+shell.exec('git log', { silent: true }, (code, stdout) => {
+  console.log(`data-${code}`);
+  console.log(`data-${typeof stdout}`);
+  console.log(`data-${stdout}`);
+  logStr = stdout;
 });
+
+// export default {
+//   logStr,
+// };
+module.exports.defalut = {
+  logStr,
+};
+// shell.ls('.').forEach((file) => {
+//   console.log(file);
+// });
+// const child = shelljs.exec('ls', { async: true });
+// child.stdout.on('data', (data) => {
+//   console.log(data);
+// });
